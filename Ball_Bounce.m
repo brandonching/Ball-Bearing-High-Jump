7fb9260cde203eedb8b434f69504d29c8eabd096
Brandon Ching <68457375+brandonching@users.noreply.github.com>
2022-04-27 11:57:26 -0600

    Finalized Code

--- Ball_Bounce.m
+++ Ball_Bounce.m
@@ -3,15 +3,16 @@ clc; clear; close all;
 
 %% Initial Setup
 E = 0.894153; % COR of ball/plate
-h = 0.5; % height at which ball is droped from above impact point
-obj_x = 0.454025; %x length of the object placement from impact
-obj_y =0.060325; %y height of the object placement from impact
-alpha = 0:0.1:90;
+h = 0.548; % height at which ball is droped from above impact point
+obj_x = 0.456; %x length of the object placement from impact
+obj_y =0.062; %y height of the object placement from impact
+alpha = 0:0.1:90; %vary the plate angle from flat to vertical
 
 % Setup parameters to model projectile with drag
+r = 0.0065; % Radius of ball
 g = 9.81; % g, gravity, m/s^2
-m = 0.523598776 * 0.00114; % m, mass, kg
-A = pi*0.01^2; % A, drag area, m^2
+m = (4/3)*pi*r^3 * 1150; % m, mass, kg (1150kg/m^3 for nylon)
+A = pi*r^2; % A, drag area, m^2
 cd = 0.5; % cd, dimensionless drag coefficient
 rho = 1.01; % rho, air density of denver, kg/m^3
 y0 = 0; % y, initial vertical position, m
@@ -27,7 +28,7 @@ cleared =[];
 % iterate through all the angles, alpha for the plate
 for a = alpha
     v = (E * cosd(a) * sqrt(2*g*h)) / (sind(acotd(E*tand(a)))); % v, initial velocity
-    angle = acotd(E*tand(a)) - a; % angle, launch angle
+    angle = atand(E*cotd(a)) - a; % angle, launch angle
     
     [x,y,vx,vy,t] = projectile_motion_drag(v,angle,g,m,A,cd,rho,y0,x0);
     max_height = [max_height max(y)]; % get max height of projectile
@@ -40,20 +41,29 @@ for a = alpha
     end
 end
 
+% plot best path
+a = min(alpha(logical(cleared)));
+v = (E * cosd(a) * sqrt(2*g*h)) / (sind(acotd(E*tand(a)))); % v, initial velocity
+angle = atand(E*cotd(a)) - a; % angle, launch angle
+    
+[x,y,vx,vy,t] = projectile_motion_drag(v,angle,g,m,A,cd,rho,y0,x0);
+plot(x,y, 'b', 'LineWidth', 2);
+
 % plot the obstacle location and labels
 plot(obj_x,obj_y, 'b.', 'MarkerSize', 30); 
 xlabel('Horizontal Distance (m)')
 ylabel('Verticle Distance (m)')
 title('Ball Projectile Motion Simulated Paths (All Angles 0-90deg)')
 set(gca,'FontSize',14)
-axis([0 0.75 0 0.4])
+axis([0 0.9 0 0.45])
 
 % create custom legend
-h=zeros(3,1); 
+h=zeros(4,1); 
 h(1) = plot(NaN,NaN,'r', 'LineWidth', 2);
 h(2) = plot(NaN,NaN,'g', 'LineWidth', 2);
-h(3) = plot(NaN,NaN,'b.', 'MarkerSize', 30);
-legend(h, 'Failed Path','Sucessful Path','Obstacle');
+h(3) = plot(NaN,NaN,'b', 'LineWidth', 2);
+h(4) = plot(NaN,NaN,'b.', 'MarkerSize', 30);
+legend(h, 'Failed Path','Sucessful Path','Best Path', 'Obstacle');
 
 %% Generate and Plot agregated data for angle vs max height
 nexttile
@@ -66,5 +76,5 @@ title('Affect of Angle on Max Height of Ball')
 legend('Does not Clear Obstacle','Clears Obstacle');
 set(gca,'FontSize',14)
 %% Print out a summary of the data
-fprintf("With a COR of e=%f, this plate/ball combination can clear an obstacle %fm away\nand %fm high when the plate angle is between %.1f-%.1f degrees.\n",E,obj_x,obj_y, min(alpha(logical(cleared))), max(alpha(logical(cleared))))
+fprintf("With a COR of e=%f, this plate/ball combination can clear an obstacle %.3fm away\nand %.3fm high when the plate angle is between %.1f-%.1f degrees.\n",E,obj_x,obj_y, min(alpha(logical(cleared))), max(alpha(logical(cleared))))
 fprintf("The best angle to clear the obstacle & maximize height is %.1f degrees with a height of %fm.\n", min(alpha(logical(cleared))), max(max_height(logical(cleared))))
